026036a5ee6e9a707aaf31c53d9e85c75793b5db
WayneMusungu <waynemuyera17@gmail.com>
2023-01-21 09:12:18 +0300

    Implement User Model

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'core.apps.CoreConfig',
 ]
 
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Setting Auth_User_Model configuration
+AUTH_USER_MODEL = 'core.User'
